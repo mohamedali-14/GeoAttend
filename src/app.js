4bce14824c28ca81b0cbce92a7e5670e0864a672
ahmedhussien4825-cloud <ahmedhussien4825@gmail.com>
2026-02-28 08:56:22 +0200

    add

--- src/app.js
+++ src/app.js
@@ -2,23 +2,21 @@ const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
 
-// إنشاء التطبيق
+
 const app = express();
 
-// اتصال بقاعدة البيانات
-connectDB();  // لازم يكون فيه console.log("MongoDB Connected") في connectDB
 
-// Middleware
+connectDB();  
+
+
 app.use(express.json());
 app.use(cors());
 
-// Routes
+
 app.use("/api/auth", require("./routes/auth"));
 
-// Test route
 app.get("/", (req, res) => res.send("API Running..."));
 
-// تشغيل السيرفر مباشرة من app.js
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/geo_attendance");
-        console.log("MongoDB Connected");
-    } catch (error) {
-        console.error(error.message);
-        process.exit(1);
-    }  
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/geo_attendance");
+    console.log("MongoDB Connected ✅");
+  } catch (error) {
+    console.error("DB Error ❌", error.message);
+    process.exit(1);
+  }
 };
 
+module.exports = connectDB;
 module.exports = connectDB;
