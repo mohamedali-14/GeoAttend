13fed3e5c5b38fabcb9090ca222e3c9e980b666f
ahmedhussien4825-cloud <ahmedhussien4825@gmail.com>
2026-02-28 03:47:03 +0200

    Add password hashing and comparison methods

--- src/models/User.js
+++ src/models/User.js
@@ -32,14 +32,14 @@ const userSchema = new mongoose.Schema({
     timestamps: true
 });
 
-// Hash password before saving
+
 userSchema.pre('save', async function(next) {
     if (!this.isModified('password')) return next();
     this.password = await bcrypt.hash(this.password, 10);
     next();
 });
 
-// Compare password method
+
 userSchema.methods.comparePassword = async function(candidatePassword) {
     return await bcrypt.compare(candidatePassword, this.password);
 };
