394ab92cdaa1d90a9e79fd04eb09f394451390e1
OmarMekky5000 <omarmekky5000@gmail.com>
2026-03-04 19:53:47 +0200

    StudentDashboard(HomePage)

---
+++ _layout.tsx
@@ -0,0 +1,16 @@
+import { Stack } from 'expo-router';
+import { AuthProvider } from '../context/AuthContext';
+
+export default function RootLayout() {
+  return (
+    <AuthProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="RoleSelectionScreen" />
+        <Stack.Screen name="LoginScreen" />
+        <Stack.Screen name="RegisterScreen" />
+        <Stack.Screen name="HomeScreen" />        {/* الجديد */}
+        <Stack.Screen name="ProfileScreen" />
+      </Stack>
+    </AuthProvider>
+  );
+}
