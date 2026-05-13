0c034f9f4e325e411fbd193dd99cc13782c0ff04
mohamedali-14 <mohamedali60062@gmail.com>
2026-05-13 17:08:36 +0300

    clean api

--- Frontend/Ziad desoky/api/modules/users/users.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { authenticateUser, requireRole } = require("../../middleware/auth.middleware");
-const { getUsers, getUser } = require("./users.controller");
-
-router.get("/", authenticateUser, requireRole("ADMIN"), getUsers);
-router.get("/:id", authenticateUser, getUser);
-
-module.exports = router;
